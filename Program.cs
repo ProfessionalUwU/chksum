23f8158ebfec337c3f42a7921dff6b8430363e60
ProfessionalUwU <andreas.fuhry@gmail.com>
2023-05-16 22:54:19 +0200

    Remove old comment

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿// Go into folder
-// Check if any file is in there
-// If there is a file. Calculate md5sum > filename.md5
-// If there is no file. Repeat
-public class Program {
+﻿public class Program {
     static void Main(string[] args) {
         
         Console.ForegroundColor = ConsoleColor.Green;
